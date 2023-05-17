613bd718e1c7d4aeb38187beab7f70f47333161a
yarosshest <yarosshest@gmail.com>
2023-05-17 23:15:33 +0300

    Telegram key move to config

--- debug_bot/debug_telegram_bot.py
+++ debug_bot/debug_telegram_bot.py
@@ -6,8 +6,9 @@ import re
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 from database.async_db import asyncHandler
 from database.db_init import db_init
+from database.config import TELEGRAMM
 
-bot = TeleBot('6269654129:AAHAw3qqV6dJsEB5woAqaKlMzceOUxUikOo')
+bot = TeleBot(TELEGRAMM)
 
 
 def split_list(lst: list, n: int):
