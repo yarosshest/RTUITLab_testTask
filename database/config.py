78cd4fef671f1eaf97402435202c15c9cfd62d74
yarosshest <yarosshest@gmail.com>
2023-05-17 23:14:32 +0300

    add json with raw db

--- database/config.py
+++ database/config.py
@@ -1,3 +1,4 @@
 parsing = False
+TELEGRAMM = "6269654129:AAHAw3qqV6dJsEB5woAqaKlMzceOUxUikOo"
 bd_connection = "postgresql+asyncpg://postgres:postgres@localhost:5432/recomapi_as"
-datajson = "KinopoiskDumb.json"
+datajson = "Kinopois.json"
